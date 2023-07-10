2a6ca09804e0623fca4146e638b728c1f14de5b5
Welber Nogueira <117238207+welbernogueira@users.noreply.github.com>
2023-07-10 20:22:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const idade = prompt("Informe sua idade:");
+
+if (idade >= 18) {
+  alert("Você é maior de idade!");
+} else if (idade > 12) {
+  alert("Você é menor de idade!");
+} else idade > 4;
+alert("Você é uma criança!");
